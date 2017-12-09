e503ba3daff5634acfeaba1277d16469c86e01d4
Stefano Fogarollo <sirfoga@protonmail.com>
2017-12-09 09:52:54 +0100

    add scan utils and bone alg

---
+++ gamer/gamer.py
@@ -0,0 +1,26 @@
+from threading import Thread
+from time import sleep
+
+
+def threaded_function(counter, thread_name):
+    for i in range(counter):
+        print thread_name, "is RUNNING counter", i
+        sleep(1)  # << calculations here
+        print thread_name, "has DONE counter", i
+
+
+if __name__ == "__main__":
+    threads = [
+        Thread(
+            target=threaded_function,
+            args=(5, thread_num)
+        ) for thread_num in range(4)
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print "Threads finished...exiting"
---
+++ gamer/utils.py
@@ -0,0 +1,33 @@
+import os
+
+
+def get_name_ext(path):
+    return os.path.splitext(path)
+
+
+def is_hidden(path):
+    name, extension = get_name_ext(path)
+    return name.startswith(".")
+
+
+def ls_recurse(path, include_hidden=False):
+    """
+    :param path: string
+        Path to directory to get list of files and folders
+    :param include_hidden: bool
+        Whether to include hidden files in list.
+    :return: list
+        List of paths in given directory recursively.
+    """
+
+    lst = []
+    for f in os.listdir(path):
+        hidden_file = is_hidden(f)
+        if (hidden_file and include_hidden) or (not hidden_file):
+            lst.append(os.path.join(path, f))
+            if os.path.isdir(os.path.join(path, f)):
+                lst += ls_recurse(
+                    os.path.join(path, f),
+                    include_hidden=include_hidden
+                )  # get list of files in directory
+    return lst
