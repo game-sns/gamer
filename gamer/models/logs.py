1d06ae997fde12fa024b240a51b41672cbd72edf
sirfoga <sirfoga@protonmail.com>
2019-03-27 07:04:41 +0100

    add file logger

--- gamer/models/logs.py
+++ gamer/models/logs.py
@@ -3,6 +3,7 @@
 
 """ Log stuff """
 
+import os
 from datetime import datetime
 
 from gamer.utils.misc import get_actual_class_name
@@ -10,12 +11,21 @@ from gamer.utils.misc import get_actual_class_name
 LOG_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
+def now():
+    return datetime.now().strftime(LOG_TIME_FORMAT)
+
+
 class Logger:
     """ Logs itself """
 
-    def __init__(self, verbose):
+    OUT_FOLDER = '/opt/game/gamer/'
+
+    def __init__(self, verbose, to_file=True):
         self.class_name = get_actual_class_name(self)
         self.verbose = bool(verbose)
+        self.to_file = bool(to_file)
+
+        self.out_file = os.path.join(self.OUT_FOLDER, now() + '.log')
 
     def log(self, *content):
         """
@@ -25,7 +35,11 @@ class Logger:
             Prints log
         """
 
+        content = " ".join([str(x) for x in content])
+        msg = '{} {} >>> {}'.format(now(), self.class_name, content)
+
         if self.verbose:
-            now = datetime.now().strftime(LOG_TIME_FORMAT)
-            content = " ".join([str(x) for x in content])
-            print now, self.class_name, ">>>", content
+            print msg
+
+        with open(self.out_file, 'a') as appender:
+            appender.write(msg + '\n')  # add new line
